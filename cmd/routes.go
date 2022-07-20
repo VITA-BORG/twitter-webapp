a4933a64f00bfd188d8c3f64a502bc8383ff8bc2
River Wang <friver980@gmail.com>
2022-07-20 16:21:10 -0400

    added schools POST route

--- cmd/routes.go
+++ cmd/routes.go
@@ -18,6 +18,7 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/schools", app.schoolAddGet)
+	router.HandlerFunc(http.MethodPost, "/schools", app.schoolAddPost)
 	router.HandlerFunc(http.MethodGet, "/users", app.users)
 	router.HandlerFunc(http.MethodGet, "/users/view/:username", app.userView)
 	router.HandlerFunc(http.MethodGet, "/users/add", app.userAddGet)
